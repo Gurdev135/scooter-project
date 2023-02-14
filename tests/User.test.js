56ed3906227bae2df89cc0bab14e60063b2fead0
Gurdev Gill <108082625+Gurdev135@users.noreply.github.com>
2023-02-14 14:34:20 +0000

    Update User.test.js

--- tests/User.test.js
+++ tests/User.test.js
@@ -1,20 +1,15 @@
 const User = require('../src/User')
 
 // User tests here
-
 // test username
-
 // test password
-
 // test age
-
 // test login
-
 // test logout
 
 describe("User Tests", () => {
     // Tests for User.js
-    test("Username & Password are valid", () => {
+    test("User values are valid", () => {
         // Creates New Instance //
         const newUser = new User("Gurdev","Password",18,false)
         // Checks if Username is valid //
@@ -25,13 +20,13 @@ describe("User Tests", () => {
         expect(newUser.password).toBeDefined()
         expect(typeof newUser.password).toBe("string")
         expect(newUser.password).toBe("Password")
-    });
-    test("Age is valid", () => {
-        // Creates New Instance //
-        const newUser = new User("Gurdev","Password",17,false)
-        //  Checks if Age is valid //
+         // Checks if Age is valid //
         expect(newUser.age).toBeDefined()
         expect(typeof newUser.age === "number" && !isNaN(newUser.age)).toBe(true);
-        
-    })
+        expect(newUser.age).toBe(18)
+        // Checks if LoggedIn is valid //
+        expect(newUser.loggedIn).toBeDefined()
+        expect(typeof newUser.loggedIn).toBe("boolean");
+        expect(newUser.loggedIn).toBe(false)
+    });
 });
